e7f7e9b2cf4efc5965322dc1fcd9c851f5a512d1
Mai-Rishi-Hoon <rishirdedhia@gmail.com>
2023-11-17 23:21:25 +0530

    book complete

--- train.js
+++ train.js
@@ -1,7 +1,5 @@
 const mysql = require('mysql');
 
-
-
 // Create a connection to the MySQL server
 const connection = mysql.createConnection({
   host: 'bkp6dfgw6yjsovpnwdie-mysql.services.clever-cloud.com',
@@ -10,94 +8,69 @@ const connection = mysql.createConnection({
   port: '3306'
 });
 
-const Og= 126;
-const Des= 104;
+const Og = 126;
+const Des = 104;
 
+// Use the database
 connection.connect((err) => {
   if (err) throw err;
-
-  // Use the database
   connection.query('USE bkp6dfgw6yjsovpnwdie', (err) => {
     if (err) throw err;
-    
+
     const Search = () => {
-      // Select data from the table
-    
-    
-    const sql1 = 'SELECT DISTINCT T.* FROM Train T WHERE T.Trainno IN ( SELECT TS1.Trainno FROM Trainstop TS1 WHERE TS1.Stationcode = 126 AND TS1.Trainno IN (SELECT TS2.Trainno FROM Trainstop TS2 WHERE TS2.Stationcode = 104))';
+      const sql1 = 'SELECT DISTINCT T.* FROM Train T WHERE T.Trainno IN ( SELECT TS1.Trainno FROM Trainstop TS1 WHERE TS1.Stationcode = 126 AND TS1.Trainno IN (SELECT TS2.Trainno FROM Trainstop TS2 WHERE TS2.Stationcode = 104))';
       connection.query(sql1, (err, results) => {
         if (err) throw err;
         console.log(results);
-
-        // Close the connection after you've obtained the results
         connection.end();
       });
-    }
+    };
 
-
-    
     const Login = () => {
-     username = 'sarang';
-     password = 'sarang123';
-     email = 'sarang@gmail.com';
-
-     const sql2 = 'Select Username from Login where Username = "sarang" and Password = "sarang123" ';
-     connection.query(sql2, (err, results) => {
-      if (err) throw err;
-      // console.log(results);
-
-      rowcount = results.length;
-      if(rowcount == 0){
-        console.log("Incorrect Username or Password !!!");
-      }
-      else{
-        console.log("Welcome ",username,"!!!");
-        Search();
-      }
-      // Close the connection after you've obtained the results
-      // connection.end();
-    });
-    }
-
+      const username = 'sarang';
+      const password = 'sarang123';
+      const email = 'sarang@gmail.com';
+      const sql2 = 'SELECT Username FROM Login WHERE Username = "sarang" AND Password = "sarang123"';
+      connection.query(sql2, (err, results) => {
+        if (err) throw err;
+        const rowcount = results.length;
+        if (rowcount === 0) {
+          console.log("Incorrect Username or Password !!!");
+        } else {
+          console.log("Welcome ", username, "!!!");
+          Search();
+        }
+      });
+    };
 
     const Signup = () => {
-      username = 'sarang';
-      password = 'sarang123';
-      email = 'sarang@gmail.com';
- 
-      const sql3 = 'Insert into Login values ("sarang","sarang@gmail.com","sarang123")';
-      connection.query(sql3, (err, results) => {
-       if (err) throw err;
-       console.log("Signup Succesfull !!!");
-      Search ();
- 
-
-       // Close the connection after you've obtained the results
-      //  connection.end();
-     });
-    }
-
+      const username = 'sarang';
+      const password = 'sarang123';
+      const email = 'sarang@gmail.com';
+      const sql3 = 'INSERT INTO Login VALUES (?, ?, ?)';
+      connection.query(sql3, [username, email, password], (err, results) => {
+        if (err) throw err;
+        console.log("Signup Successful !!!");
+        Search();
+      });
+    };
 
     const Book = () => {
-      pname = 'Ramesh Shah';
-      
-      page = 20;
-      pgender = 'M';
-      pcontactno = 1234567890;
-      // ticketno = '';
-      bookingdate = '2023-11-17';
-      journeydate = '2023-12-15';
-      jclass = "CC";
-      price = 740;
-      pid = 1;
-      // coach = '';
-      // seatno = '';
-      sstatus = 'confirmed';
-      trainno = 82902;
-      trainname = 'IRCTC Tejas Express';
+      const pname = 'Ramesh Shah';
+      const page = 20;
+      const pgender = 'M';
+      const pcontactno = 1234567890;
+      const bookingdate = '2023-11-17';
+      const journeydate = '2023-12-15';
+      const jclass = "CC";
+      const price = 740;
+      const pid = 1;
+      const sstatus = 'confirmed';
+      const trainno = 82902;
+      const trainname = 'IRCTC Tejas Express';
+      let seatno = '';
 
       function assignCoachNumber(jclass) {
-        // Valid classes and corresponding coach ranges
         const classToCoachMap = {
           '3AC': { prefix: 'B', min: 1, max: 6 },
           '2AC': { prefix: 'A', min: 1, max: 3 },
@@ -106,34 +79,23 @@ connection.connect((err) => {
           '2C': { prefix: 'D', min: 1, max: 9 },
           'CC': { prefix: 'C', min: 1, max: 9 },
           'EC': { prefix: 'E', min: 1, max: 2 }
-          // Add more classes and coach ranges as needed
         };
-      
-        // Convert the class to uppercase for case-insensitivity
+
         const normalizedClass = jclass.toUpperCase();
-      
-        // Check if the provided class is valid
+
         if (classToCoachMap.hasOwnProperty(normalizedClass)) {
-          // Get the coach range for the specified class
           const { prefix, min, max } = classToCoachMap[normalizedClass];
-      
-          // Generate a random coach number within the specified range
           const coachNumber = prefix + getRandomNumber(min, max);
-      
           return coachNumber;
         } else {
-          // Handle invalid class
           return 'Invalid Class';
         }
       }
 
-      // Helper function to generate a random number within a range
       function getRandomNumber(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
       }
 
-      // Function to generate a seatno
-      // Function to generate a seatno
       function generateSeatno(trainno, coach, callback) {
         const sn = Math.floor(Math.random() * (64 - 1 + 1)) + 1;
         check(sn, trainno, coach, function (isValid) {
@@ -144,76 +106,63 @@ connection.connect((err) => {
           }
         });
       }
-      
+
       function check(sn, trainno, coach, callback) {
         const sql6 = 'SELECT * FROM Ticket WHERE Trainno = ? AND Coach = ? AND Seat = ?';
         connection.query(sql6, [trainno, coach, sn], function (err, result) {
           if (err) {
             console.error('Error executing query:', err);
-            callback(undefined);  // Pass undefined to indicate an error
+            callback(undefined);
             return;
           }
           const res = result.length;
           callback(res === 0);
         });
       }
-      
-      // Example of using generateSeatno
-      generateSeatno('your_train_no', 'your_coach_type', function (result) {
+
+      generateSeatno(trainno, assignCoachNumber(jclass), function (result) {
         if (result !== undefined) {
           console.log('Generated Seat No:', result);
+          seatno = result;
+
+          function generatePassengerId() {
+            const timestamp = new Date().getTime();
+            const random = Math.floor(Math.random() * 9000) + 1000;
+            const passengerId = `${timestamp}${random}`.slice(0, 10);
+            return passengerId;
+          }
+
+          const reservid = generatePassengerId();
+          const coach = assignCoachNumber(jclass);
+
+          const sql4 = 'INSERT INTO Passenger (PassId, Passname, Age, Gender, Contactno) VALUES (?,?,?,?,?)';
+          connection.query(sql4, [pid, pname, page, pgender, pcontactno], (error, results) => {
+            if (error) {
+              console.error('Error executing INSERT query:', error);
+            } else {
+              console.log('Insert successful. Affected rows:', results.affectedRows);
+
+              const sql5 = 'INSERT INTO Ticket (Reservationid, Trainno, Trainname, Bookingdate, Journeydate, Class, Status, Coach, Seat, Price, PassId) VALUES (?,?,?,?,?,?,?,?,?,?,?)';
+              connection.query(sql5, [reservid, trainno, trainname, bookingdate, journeydate, jclass, sstatus, coach, seatno, price, pid], (error, results) => {
+                if (error) {
+                  console.error('Error executing INSERT query:', error);
+                } else {
+                  console.log('Insert successful. Affected rows:', results.affectedRows);
+                }
+              });
+            }
+          });
+
         } else {
           console.log('Error generating seat number.');
         }
       });
-      
+    };
 
-
-
-
-
-      function generatePassengerId() {
-        // Generate a timestamp (current time in milliseconds)
-        const timestamp = new Date().getTime();
-      
-        // Generate a random number (between 1000 and 9999)
-        const random = Math.floor(Math.random() * 9000) + 1000;
-      
-        // Combine timestamp and random number to create a unique ID
-        const passengerId = `${timestamp}${random}`;
-      
-        return passengerId;
-      }      
-
-      reservid = generatePassengerId();
-      coach = assignCoachNumber(jclass);
-      seatno = generateSeatno(trainno, coach);
-
-      const sql4 = 'INSERT INTO Passenger (PassId, Passname, Age, Gender, Contactno) VALUES (?,?,?,?,?)';
-      connection.query(sql4, [pid, pname, page, pgender, pcontactno], (error, results) => {
-        if (error) {
-          console.error('Error executing INSERT query:', error);
-        } else {
-          console.log('Insert successful. Affected rows:', results.affectedRows);
-        }
-    })
-
-    const sql5 = 'INSERT INTO Ticket (Reservationid, Trainno, Trainname, Bookingdate, Journeydate, Class, Coach, Seat, Price,	PassId) VALUES (?,?,?,?,?,?,?,?,?,?)';
-      connection.query(sql5, [reservid, trainno, trainname, bookingdate, journeydate, jclass, coach,seatno, price, pid], (error, results) => {
-        if (error) {
-          console.error('Error executing INSERT query:', error);
-        } else {
-          console.log('Insert successful. Affected rows:', results.affectedRows);
-        }
-    })
-
-
-   
-    
-  }
-  //Search();
-  //Login();
-  //Signup()
-  Book();
-});
+    // Uncomment the function you want to test
+    // Search();
+    // Login();
+    // Signup();
+    Book();
+  });
 });
