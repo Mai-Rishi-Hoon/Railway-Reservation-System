293fd2350203bfbf7c7d41373061a2c2a4d9ff30
Mai-Rishi-Hoon <rishirdedhia@gmail.com>
2023-11-17 20:59:12 +0530

    book update

--- train.js
+++ train.js
@@ -16,7 +16,7 @@ const Des= 104;
 connection.connect((err) => {
   if (err) throw err;
 
-  // Create the database
+  // Use the database
   connection.query('USE bkp6dfgw6yjsovpnwdie', (err) => {
     if (err) throw err;
     
@@ -84,16 +84,93 @@ connection.connect((err) => {
       page = 20;
       pgender = 'M';
       pcontactno = 1234567890;
-      ticketno = '';
-      bookingdate = '';
-      journeydate = '';
-      jclass = "3AC";
+      // ticketno = '';
+      bookingdate = '2023-11-17';
+      journeydate = '2023-12-15';
+      jclass = "CC";
       price = 740;
-      reservid = '';
-      seatno = '';
-      sstatus = '';
-      trainno = '';
-      trainname = '';
+      pid = 1;
+      // coach = '';
+      // seatno = '';
+      sstatus = 'confirmed';
+      trainno = 82902;
+      trainname = 'IRCTC Tejas Express';
+
+      function assignCoachNumber(jclass) {
+        // Valid classes and corresponding coach ranges
+        const classToCoachMap = {
+          '3AC': { prefix: 'B', min: 1, max: 6 },
+          '2AC': { prefix: 'A', min: 1, max: 3 },
+          '1AC': { prefix: 'HA', min: 1, max: 1 },
+          'SL': { prefix: 'S', min: 1, max: 8 },
+          '2C': { prefix: 'D', min: 1, max: 9 },
+          'CC': { prefix: 'C', min: 1, max: 9 },
+          'EC': { prefix: 'E', min: 1, max: 2 }
+          // Add more classes and coach ranges as needed
+        };
+      
+        // Convert the class to uppercase for case-insensitivity
+        const normalizedClass = jclass.toUpperCase();
+      
+        // Check if the provided class is valid
+        if (classToCoachMap.hasOwnProperty(normalizedClass)) {
+          // Get the coach range for the specified class
+          const { prefix, min, max } = classToCoachMap[normalizedClass];
+      
+          // Generate a random coach number within the specified range
+          const coachNumber = prefix + getRandomNumber(min, max);
+      
+          return coachNumber;
+        } else {
+          // Handle invalid class
+          return 'Invalid Class';
+        }
+      }
+
+      // Helper function to generate a random number within a range
+      function getRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+
+      // Function to generate a seatno
+      // Function to generate a seatno
+      function generateSeatno(trainno, coach, callback) {
+        const sn = Math.floor(Math.random() * (64 - 1 + 1)) + 1;
+        check(sn, trainno, coach, function (isValid) {
+          if (isValid || isValid === undefined) {
+            callback(sn);
+          } else {
+            generateSeatno(trainno, coach, callback);
+          }
+        });
+      }
+      
+      function check(sn, trainno, coach, callback) {
+        const sql6 = 'SELECT * FROM Ticket WHERE Trainno = ? AND Coach = ? AND Seat = ?';
+        connection.query(sql6, [trainno, coach, sn], function (err, result) {
+          if (err) {
+            console.error('Error executing query:', err);
+            callback(undefined);  // Pass undefined to indicate an error
+            return;
+          }
+          const res = result.length;
+          callback(res === 0);
+        });
+      }
+      
+      // Example of using generateSeatno
+      generateSeatno('your_train_no', 'your_coach_type', function (result) {
+        if (result !== undefined) {
+          console.log('Generated Seat No:', result);
+        } else {
+          console.log('Error generating seat number.');
+        }
+      });
+      
+
+
+
+
 
       function generatePassengerId() {
         // Generate a timestamp (current time in milliseconds)
@@ -108,13 +185,35 @@ connection.connect((err) => {
         return passengerId;
       }      
 
-      pid = generatePassengerId();
-    }
+      reservid = generatePassengerId();
+      coach = assignCoachNumber(jclass);
+      seatno = generateSeatno(trainno, coach);
+
+      const sql4 = 'INSERT INTO Passenger (PassId, Passname, Age, Gender, Contactno) VALUES (?,?,?,?,?)';
+      connection.query(sql4, [pid, pname, page, pgender, pcontactno], (error, results) => {
+        if (error) {
+          console.error('Error executing INSERT query:', error);
+        } else {
+          console.log('Insert successful. Affected rows:', results.affectedRows);
+        }
+    })
+
+    const sql5 = 'INSERT INTO Ticket (Reservationid, Trainno, Trainname, Bookingdate, Journeydate, Class, Coach, Seat, Price,	PassId) VALUES (?,?,?,?,?,?,?,?,?,?)';
+      connection.query(sql5, [reservid, trainno, trainname, bookingdate, journeydate, jclass, coach,seatno, price, pid], (error, results) => {
+        if (error) {
+          console.error('Error executing INSERT query:', error);
+        } else {
+          console.log('Insert successful. Affected rows:', results.affectedRows);
+        }
+    })
 
 
-   //Search();
+   
+    
+  }
+  //Search();
   //Login();
   //Signup()
-    
-  });
+  Book();
+});
 });
